638edd8a04f8a1ce2c1e65135ecf138e767795e4
kyren <kerriganw@gmail.com>
2015-06-20 19:44:15 +0100

    better tests

--- test/Gameboy/AssemblerSpec.hs
+++ test/Gameboy/AssemblerSpec.hs
@@ -8,35 +8,23 @@ import Gameboy.Instructions
 import Gameboy.Assembler
 import Text.Heredoc
 
-type AssemblyPassTest = (String, String, [Instruction])
-type AssemblyFailTest = (String, [String])
-
-testLoad8 :: AssemblyPassTest
-testLoad8 = (
-    "parses 8 bit loads",
+testLoad8 :: Spec
+testLoad8 = it "parses 8 bit loads" $
+  parseInstructions
     [here|
       LD B,$ff; comment
       LD     C, (HL)
       ; comment
-    |],
-    [
+    |] `shouldBe` Right [
       LD_R_N BRegister 0xff,
       LD_R_ATHL CRegister
     ]
-  )
 
-testInvalidLoad8 :: AssemblyFailTest
-testInvalidLoad8 = (
-    "does not parse invalid 8 bit loads",
-    [
-      "LD (HL), (HL)"
-    ]
-  )
+testInvalidLoad8 :: Spec
+testInvalidLoad8 = it "does not parse invalid 8 bit loads" $
+  parseInstructions "LD (HL), (HL)" `shouldSatisfy` isLeft
 
 spec :: Spec
 spec = describe "assembly" $ do
-    mapM_ runPassTest [testLoad8]
-    mapM_ runFailTest [testInvalidLoad8]
-  where
-    runPassTest (desc, asm, inst) = it desc $ parseInstructions asm `shouldBe` Right inst
-    runFailTest (desc, asms) = it desc $ all isLeft $ map parseInstructions asms
+  testLoad8
+  testInvalidLoad8
--- test/ProgramSpec.hs
+++ test/ProgramSpec.hs
@@ -8,39 +8,33 @@ import Gameboy.Assembler
 import Gameboy.TestState
 import Text.Heredoc
 
-type MemCheck = [(Word16, Word8)]
-type ProgramTest = (String, String, MemCheck)
+runTest :: String -> [(Word16, Word8)] -> Expectation
+runTest asm checks =
+  case assemble asm of
+    Right prog -> testProgram prog checks `shouldBe` True
+    Left er -> error er
 
-testNoOps :: ProgramTest
-testNoOps = (
-    "runs no ops correctly",
-    [here|
+testNoOps :: Spec
+testNoOps = it "runs no ops correctly" $
+  runTest [here|
       NOP
       NOP
       NOP
       NOP
       STOP
-    |],
-    [(0xff, 0)]
-  )
+    |] [(0xff, 0)]
 
-test8BitLoads :: ProgramTest
-test8BitLoads = (
-    "does 8 bit loads properly",
-    [here|
+test8BitLoads :: Spec
+test8BitLoads = it "does 8 bit loads properly" $
+  runTest [here|
       LD B,$07
       LD H,$00
       LD L,$f8
       LD (HL),B
       STOP
-    |],
-    [(0xf7, 0), (0xf8, 7), (0xf9, 0)]
-  )
+    |] [(0xf7, 0), (0xf8, 7), (0xf9, 0)]
 
 spec :: Spec
-spec = describe "emulation" $ mapM_ runTest [testNoOps, test8BitLoads]
-  where
-    runTest (desc, asm, checks) = it desc $
-      case assemble asm of
-        Right prog -> testProgram prog checks `shouldBe` True
-        Left er -> error er
+spec = describe "emulation" $ do
+  testNoOps
+  test8BitLoads

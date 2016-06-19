4d4bac9a5a1fbedf71e6f5f05e05d1d5e0cd1849
kyren <kerriganw@gmail.com>
2016-06-19 05:16:05 +0100

    much simpler and faster with ghc 8.0
    removed 'emulator' stub project, as sdl2 is not yet available on
    stackage nightly, and it didn't do anything anyway

--- lib/Gameboy/Instructions.hs
+++ lib/Gameboy/Instructions.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE StrictData #-}
 
 module Gameboy.Instructions (
   Bit(..),
@@ -54,22 +54,22 @@ data ResetAddress
   deriving (Show, Eq)
 
 data Instruction
-  = LD_R_R !Register !Register
-  | LD_R_N !Register !Word8
-  | LD_R_ATHL !Register
+  = LD_R_R Register Register
+  | LD_R_N Register Word8
+  | LD_R_ATHL Register
 
-  | LD_ATHL_R !Register
-  | LD_ATHL_N !Word8
+  | LD_ATHL_R Register
+  | LD_ATHL_N Word8
 
   | LD_A_ATC
   | LD_A_ATBC
   | LD_A_ATDE
-  | LD_A_ATNN !Word16
+  | LD_A_ATNN Word16
 
   | LD_ATC_A
   | LD_ATBC_A
   | LD_ATDE_A
-  | LD_ATNN_A !Word16
+  | LD_ATNN_A Word16
 
   | LDD_A_ATHL
   | LDD_ATHL_A
@@ -77,17 +77,17 @@ data Instruction
   | LDI_A_ATHL
   | LDI_ATHL_A
 
-  | LDH_A_ATN !Word8
-  | LDH_ATN_A !Word8
+  | LDH_A_ATN Word8
+  | LDH_ATN_A Word8
 
-  | LD_BC_NN !Word16
-  | LD_DE_NN !Word16
-  | LD_HL_NN !Word16
-  | LD_SP_NN !Word16
+  | LD_BC_NN Word16
+  | LD_DE_NN Word16
+  | LD_HL_NN Word16
+  | LD_SP_NN Word16
 
   | LD_SP_HL
-  | LDHL_SP_N !Int8
-  | LD_ATNN_SP !Word16
+  | LDHL_SP_N Int8
+  | LD_ATNN_SP Word16
 
   | PUSH_AF
   | PUSH_BC
@@ -99,42 +99,42 @@ data Instruction
   | POP_DE
   | POP_HL
 
-  | ADD_A_R !Register
-  | ADD_A_N !Word8
+  | ADD_A_R Register
+  | ADD_A_N Word8
   | ADD_A_ATHL
 
-  | ADC_A_R !Register
-  | ADC_A_N !Word8
+  | ADC_A_R Register
+  | ADC_A_N Word8
   | ADC_A_ATHL
 
-  | SUB_R !Register
-  | SUB_N !Word8
+  | SUB_R Register
+  | SUB_N Word8
   | SUB_ATHL
 
-  | SBC_A_R !Register
-  | SBC_A_N !Word8
+  | SBC_A_R Register
+  | SBC_A_N Word8
   | SBC_A_ATHL
 
-  | AND_R !Register
-  | AND_N !Word8
+  | AND_R Register
+  | AND_N Word8
   | AND_ATHL
 
-  | OR_R !Register
+  | OR_R Register
   | OR_ATHL
-  | OR_N !Word8
+  | OR_N Word8
 
-  | XOR_R !Register
-  | XOR_N !Word8
+  | XOR_R Register
+  | XOR_N Word8
   | XOR_ATHL
 
-  | CP_R !Register
-  | CP_N !Word8
+  | CP_R Register
+  | CP_N Word8
   | CP_ATHL
 
-  | INC_R !Register
+  | INC_R Register
   | INC_ATHL
 
-  | DEC_R !Register
+  | DEC_R Register
   | DEC_ATHL
 
   | ADD_HL_BC
@@ -142,7 +142,7 @@ data Instruction
   | ADD_HL_HL
   | ADD_HL_SP
 
-  | ADD_SP_N !Int8
+  | ADD_SP_N Int8
 
   | INC_BC
   | INC_DE
@@ -154,7 +154,7 @@ data Instruction
   | DEC_HL
   | DEC_SP
 
-  | SWAP_R !Register
+  | SWAP_R Register
   | SWAP_ATHL
 
   | DAA
@@ -173,50 +173,50 @@ data Instruction
   | RRCA
   | RRA
 
-  | RLC_R !Register
+  | RLC_R Register
   | RLC_ATHL
 
-  | RL_R !Register
+  | RL_R Register
   | RL_ATHL
 
-  | RRC_R !Register
+  | RRC_R Register
   | RRC_ATHL
 
-  | RR_R !Register
+  | RR_R Register
   | RR_ATHL
 
-  | SLA_R !Register
+  | SLA_R Register
   | SLA_ATHL
 
-  | SRA_R !Register
+  | SRA_R Register
   | SRA_ATHL
 
-  | SRL_R !Register
+  | SRL_R Register
   | SRL_ATHL
 
-  | BIT_B_R !Bit !Register
-  | BIT_B_ATHL !Bit
+  | BIT_B_R Bit Register
+  | BIT_B_ATHL Bit
 
-  | SET_B_R !Bit !Register
-  | SET_B_ATHL !Bit
+  | SET_B_R Bit Register
+  | SET_B_ATHL Bit
 
-  | RES_B_R !Bit !Register
-  | RES_B_ATHL !Bit
+  | RES_B_R Bit Register
+  | RES_B_ATHL Bit
 
-  | JP_NN !Word16
-  | JP_C_NN !Cond !Word16
+  | JP_NN Word16
+  | JP_C_NN Cond Word16
   | JP_ATHL
 
-  | JR_N !Int8
-  | JR_C_N !Cond !Int8
+  | JR_N Int8
+  | JR_C_N Cond Int8
 
-  | CALL_NN !Word16
-  | CALL_C_NN !Cond !Word16
+  | CALL_NN Word16
+  | CALL_C_NN Cond Word16
 
-  | RST_RA !ResetAddress
+  | RST_RA ResetAddress
 
   | RET
-  | RET_C !Cond
+  | RET_C Cond
 
   | RETI
 
